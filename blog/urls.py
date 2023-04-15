2f0992a8f466106ea718de61eab0980e1942075e
Mateusz Miśkiewicz <br.miskiewicz@gmail.com>
2023-04-15 18:03:11 +0200

    update urls.py file

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("posts", views.posts),
-    path("posts/<int:post_id>", views.post),
+    path("", views.index, name="index"),
+    path("posts", views.index),
+    path("posts/<slug:slug>", views.post, name="post"),
 ]
