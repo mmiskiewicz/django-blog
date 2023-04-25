7f5997f0574dfe5b0b1588d0ebe0b290a77b2191
Mateusz Miśkiewicz <br.miskiewicz@gmail.com>
2023-04-25 14:33:52 +0200

    add allowed hosts

--- my_site/settings.py
+++ my_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["my-blog-4m3q.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
