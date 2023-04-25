01f44c32539ee7e6cdd7629a0f7cdd4d4858e9a8
Mateusz Miśkiewicz <br.miskiewicz@gmail.com>
2023-04-25 17:29:18 +0200

    add static_root

--- my_site/settings.py
+++ my_site/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["my-blog-4m3q.onrender.com", "127.0.0.1", "localhost"]
 
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static"
