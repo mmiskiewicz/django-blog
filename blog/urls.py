53189d7460b1412ff0d7d2d264d7afcb2f775a9c
Mateusz Miśkiewicz <br.miskiewicz@gmail.com>
2023-04-25 17:29:54 +0200

    add static url

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("posts", views.index),
     path("posts/<slug:slug>", views.post, name="post"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
