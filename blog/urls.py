23b66fea62cafafa0647c951b85e9b04be910107
Mateusz Miśkiewicz <br.miskiewicz@gmail.com>
2023-04-29 19:53:22 +0200

    remove static urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path, include
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("posts", views.index),
     path("posts/<slug:slug>", views.post, name="post-page"),
     path("favorites", views.favorites, name="favorites"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
