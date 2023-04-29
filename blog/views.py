205fcbf6c99776eb841d4af8b6d580b1f41da1a8
Mateusz Miśkiewicz <br.miskiewicz@gmail.com>
2023-04-29 18:10:03 +0200

    add comment form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator
+from .forms import CommentForm
+from django.views import View
+from django.views.generic import ListView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -25,6 +30,16 @@ def posts(request):
 def post(request, slug):
     # identified_post = next(post for post in posts_list if post["slug"] == slug)
     identified_post = get_object_or_404(Post, slug=slug)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = identified_post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-page", args=[slug]))
     return render(request, "blog/post.html", {
         "post": identified_post,
+        "form": form,
+        "comments": identified_post.comments.all().order_by("-id"),
     })
