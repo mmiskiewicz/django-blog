b9e2cc5ff19a406ee3dfd2132febfce1534a6cb5
Mateusz Miśkiewicz <br.miskiewicz@gmail.com>
2023-04-29 19:52:05 +0200

    add additional options to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author, Tag, Post
+from .models import Author, Tag, Post, Comment
 
 # Register your models here.
 
@@ -10,6 +10,10 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "date", "author", )
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+
 admin.site.register(Author)
 admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
