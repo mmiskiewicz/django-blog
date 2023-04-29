fa3c3e09cb679e11fd31d4cdedeeaf60fedcdfaf
Mateusz Miśkiewicz <br.miskiewicz@gmail.com>
2023-04-29 19:32:19 +0200

    add 'remove from favorites' functionality

--- blog/views.py
+++ blog/views.py
@@ -30,6 +30,11 @@ def posts(request):
 def post(request, slug):
     # identified_post = next(post for post in posts_list if post["slug"] == slug)
     identified_post = get_object_or_404(Post, slug=slug)
+    stored_posts = request.session.get("stored_posts")
+    if stored_posts is not None:
+        is_marked_as_favorite = identified_post.id in stored_posts
+    else:
+        is_marked_as_favorite = False
     form = CommentForm()
     if request.method == "POST":
         form = CommentForm(request.POST)
@@ -42,4 +47,37 @@ def post(request, slug):
         "post": identified_post,
         "form": form,
         "comments": identified_post.comments.all().order_by("-id"),
+        "is_marked_as_favorite": is_marked_as_favorite,
+    })
+
+
+def favorites(request):
+    stored_posts = request.session.get("stored_posts")
+    context = {}
+
+    if stored_posts is None or len(stored_posts) == 0:
+        stored_posts = []
+        posts = None
+    else:
+        posts = Post.objects.filter(id__in=stored_posts)
+        p = Paginator(posts, 3)
+        page = request.GET.get("page")
+        posts = p.get_page(page)
+
+
+    if request.method == "POST":
+
+        post_id = int(request.POST["post_id"])
+
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+        else:
+            stored_posts.remove(post_id)
+
+        request.session["stored_posts"] = stored_posts
+        return HttpResponseRedirect("/")
+
+    return render(request, "blog/favorite-posts.html", {
+        "context": context,
+        "posts": posts,
     })
