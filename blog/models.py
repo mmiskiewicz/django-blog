0ffe1535956eee5d6df1388de87341f9509f2dab
Mateusz Miśkiewicz <br.miskiewicz@gmail.com>
2023-04-29 18:08:10 +0200

    add comment model

--- blog/models.py
+++ blog/models.py
@@ -32,3 +32,10 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title}"
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=100)
+    user_email = models.EmailField()
+    comment_text = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
