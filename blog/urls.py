42a0d198d70bb208a8b7959590dc9aabeb01363b
Mateusz Miśkiewicz <br.miskiewicz@gmail.com>
2023-04-29 19:33:18 +0200

    add path to favorites

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ from django.conf import settings
 urlpatterns = [
     path("", views.index, name="index"),
     path("posts", views.index),
-    path("posts/<slug:slug>", views.post, name="post"),
+    path("posts/<slug:slug>", views.post, name="post-page"),
+    path("favorites", views.favorites, name="favorites"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
